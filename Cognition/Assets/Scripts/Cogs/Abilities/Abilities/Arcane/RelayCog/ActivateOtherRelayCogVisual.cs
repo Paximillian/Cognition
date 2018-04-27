f77b28143917414caca7b5e1efeff66a180767b6
Memilmon <emilatron@gmail.com>
2018-04-27 09:22:17 +0300

    no message

--- Cognition/Assets/Scripts/Cogs/Abilities/Abilities/Arcane/RelayCog/ActivateOtherRelayCogVisual.cs
+++ Cognition/Assets/Scripts/Cogs/Abilities/Abilities/Arcane/RelayCog/ActivateOtherRelayCogVisual.cs
@@ -34,6 +34,9 @@ public class ActivateOtherRelayCogVisual : CogAbility, IGameMechanicAbility
             yield return null;
         }
 
+        //Yes we need this! Without it the cogs salute
+        yield return null;
+        yield return null;
         if (invokingCog.HasSameOwnerAs(TriggeringCog))
         {
             (invokingCog as RelayCog)?.ActivateRelayEffect(TriggeringCog);
