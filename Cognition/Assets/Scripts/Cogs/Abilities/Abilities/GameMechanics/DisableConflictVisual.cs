13b2d8db809ae040c2631af6c0de9d6c699f3b46
Memilmon <emilatron@gmail.com>
2018-05-06 23:00:08 +0300

    no message

---
+++ Cognition/Assets/Scripts/Cogs/Abilities/Abilities/GameMechanics/DisableConflictVisual.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableConflictVisual : CogAbility, IGameMechanicAbility
+{
+
+    public override string Description
+    {
+        get
+        {
+            return base.Description + "Deactivates the visual representation of a conflict.";
+        }
+    }
+
+    protected override bool canTrigger()
+    {
+        return true;
+    }
+
+    protected override void triggerLogic(Cog invokingCog)
+    {
+        //Empty on purpose
+    }
+
+    protected override void triggerVisuals(Cog invokingCog)
+    {
+        TriggeringCog.StopConflictEffect();
+    }
+}
