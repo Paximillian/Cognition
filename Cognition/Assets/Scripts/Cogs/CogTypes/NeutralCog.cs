ccf13a1abf0e7bc6870f0ba8e728580448c21870
Memilmon <emilatron@gmail.com>
2018-08-04 16:49:24 +0300

    Fixed a propogation bug

--- Cognition/Assets/Scripts/Cogs/CogTypes/NeutralCog.cs
+++ Cognition/Assets/Scripts/Cogs/CogTypes/NeutralCog.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public class NeutralCog : Cog
 {
     public static HashSet<NeutralCog> NeutralCogs { get; private set; } = new HashSet<NeutralCog>();
+
+    public override Func<Cog, bool> HasSameOwnerAs => ((i_AskingCog) => i_AskingCog == null ? false :
+                                                                        (OccupyingPlayers.Contains((i_AskingCog as PlayableCog)?.OwningPlayer)));
 
     private void OnEnable()
     {
